7f65f00789675427d27f6771048b73d1d70d1d8c
andrewarosario <andrew.arosario@gmail.com>
2022-03-14 09:49:54 -0300

    build activate logic

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+enum YesNoButtonGroupOptions {
+  Yes = 'yes',
+  No = 'no'
+}
+
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
@@ -7,12 +12,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class YesNoButtonGroupComponent implements OnInit {
 
-  @Input() public value: string = null;
+  @Input() public value: YesNoButtonGroupOptions = null;
   @Input() public label = '';
+  public options = YesNoButtonGroupOptions;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public activate(value: YesNoButtonGroupOptions) {
+    this.value = value;
   }
 
 }
