e5a5c559246abe0e3d5fc6400d2fb19adc5006e1
andrewarosario <andrew.arosario@gmail.com>
2022-03-14 10:53:59 -0300

    implement ControlValueAcessor

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 enum YesNoButtonGroupOptions {
   Yes = 'yes',
@@ -10,16 +11,30 @@ enum YesNoButtonGroupOptions {
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss']
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements ControlValueAccessor {
 
   @Input() public value: YesNoButtonGroupOptions = null;
   @Input() public label = '';
   @Output() public valueChange = new EventEmitter<YesNoButtonGroupOptions>();
   public options = YesNoButtonGroupOptions;
+  public onChange = (value: YesNoButtonGroupOptions) => { };
+  public onTouched = () => {};
 
-  constructor() { }
+  public writeValue(value: YesNoButtonGroupOptions): void {
+    this.value = value;
+    this.onChange(this.value);
+  }
 
-  ngOnInit(): void {
+  public registerOnChange(fn: (value: YesNoButtonGroupOptions) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
   }
 
   public activate(value: YesNoButtonGroupOptions) {
