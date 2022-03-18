bd4ace0b244ffe2483f67892d3969839b090c80a
andrewarosario <andrew.arosario@gmail.com>
2022-03-18 11:09:09 -0300

    define modal API

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,6 +1,21 @@
-import { Injectable } from "@angular/core";
+import { Injectable, TemplateRef } from "@angular/core";
+
+export interface ModalConfig {
+    templateRef: TemplateRef<any>,
+    title: string
+}
 
 @Injectable()
 export class ModalService {
+    public open(config: ModalConfig) {
+        console.log('open called');
+        return new ModalRef();
+    }
+}
 
+export class ModalRef {
+    public close() {
+        console.log('close called');
+        
+    }
 }
