6890414c6e0f369978f590097175a25767486679
andrewarosario <andrew.arosario@gmail.com>
2022-03-21 12:08:10 -0300

    create fade animation in ngIf

--- src/app/accessible-modal/accessible-modal.component.ts
+++ src/app/accessible-modal/accessible-modal.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { fade } from '../shared/animations/fade';
 import { ModalRef, ModalService } from '../shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-accessible-modal',
   templateUrl: './accessible-modal.component.html',
-  styleUrls: ['./accessible-modal.component.scss']
+  styleUrls: ['./accessible-modal.component.scss'],
+  animations: [fade]
 })
 export class AccessibleModalComponent implements OnInit {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   firstName = 'Andrew';
   public modalRef: ModalRef;
+  public info = false;
 
   constructor(private modalService: ModalService) { }
 
