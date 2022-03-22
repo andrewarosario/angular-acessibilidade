0f080d6733f5bbba992f58ff8b09858581cb7f23
andrewarosario <andrew.arosario@gmail.com>
2022-03-22 12:48:43 -0300

    create focus back directive

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FocusBackModule } from "../../directives/keyboard-manager/focus-back/focus-back.module";
 import { FocusTrapModule } from "../../directives/keyboard-manager/focus-trap/focus-trap.module";
 import { ModalComponent } from "./modal.component";
 import { ModalService } from "./services/modal.service";
 
 @NgModule({
     declarations: [ModalComponent],
-    imports: [CommonModule, FocusTrapModule],
+    imports: [CommonModule, FocusTrapModule, FocusBackModule],
     exports: [ModalComponent],
     providers: [ModalService]
 })
---
+++ src/app/shared/directives/keyboard-manager/focus-back/focus-back.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FocusBackDirective } from "./focus-back.directive";
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [FocusBackDirective],
+    exports: [FocusBackDirective]
+})
+export class FocusBackModule { }
